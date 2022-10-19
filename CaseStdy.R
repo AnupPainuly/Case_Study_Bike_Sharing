b72896f809d4ebdebc00b77b66974561aa191daa
AnupPainuly <88976018+AnupPainuly@users.noreply.github.com>
2022-10-19 22:26:01 +0530

    Add files via upload

---
+++ CaseStdy.R
@@ -0,0 +1,292 @@
+install.packages("tidyverse")
+library(tidyverse)
+library(lubridate)
+library(skimr)
+install.packages("dplyr")
+library(dplyr)
+install.packages("skimr")
+install.packages("janitor")
+library(janitor)
+
+Trips_Apr20 <- read_csv('202004-divvy-tripdata.csv')
+spec(Trips_Apr20)
+Trips_May20 <- read_csv('202005-divvy-tripdata.csv')
+spec(Trips_May20)
+Trips_Jun20 <- read_csv('202006-divvy-tripdata.csv')
+spec(Trips_Jun20)
+Trips_Jul20 <- read_csv('202007-divvy-tripdata.csv')
+spec(Trips_Jul20)
+Trips_Aug20 <- read_csv('202008-divvy-tripdata.csv')
+spec(Trips_Aug20)
+Trips_Sep20 <- read_csv('202009-divvy-tripdata.csv')
+spec(Trips_Sep20)
+Trips_Oct20 <- read_csv('202010-divvy-tripdata.csv')
+spec(Trips_Oct20)
+Trips_Nov20 <- read_csv('202011-divvy-tripdata.csv')
+spec(Trips_Nov20)
+Trips_Dec20 <- read_csv('202012-divvy-tripdata.csv')
+spec(Trips_Dec20)
+Trips_Jan21 <- read_csv('202101-divvy-tripdata.csv')
+spec(Trips_Jan21)
+Trips_Feb21 <- read_csv('202102-divvy-tripdata.csv')
+spec(Trips_Feb21)
+Trips_Mar21 <- read_csv('202103-divvy-tripdata.csv')
+spec(Trips_Mar21)
+Trips_Apr21 <- read_csv('202104-divvy-tripdata.csv')
+spec(Trips_Apr21)
+
+#Compare column names in all the files
+
+colnames(Trips_Apr20)
+colnames(Trips_May20)
+colnames(Trips_Jun20)
+colnames(Trips_Jul20)
+colnames(Trips_Aug20)
+colnames(Trips_Sep20)
+colnames(Trips_Oct20)
+colnames(Trips_Nov20)
+colnames(Trips_Dec20)
+colnames(Trips_Jan21)
+colnames(Trips_Feb21)
+colnames(Trips_Mar21)
+colnames(Trips_Apr21)
+
+#Inspection of Incongruity In Data Frame
+
+str(Trips_Apr20)
+str(Trips_May20)
+str(Trips_Jun20)
+str(Trips_Jul20)
+str(Trips_Aug20)
+str(Trips_Sep20)
+str(Trips_Oct20)
+str(Trips_Nov20)
+str(Trips_Dec20)
+str(Trips_Jan21)
+str(Trips_Feb21)
+str(Trips_Mar21)
+str(Trips_Apr21)
+
+#Datatype comparison across all dataframe
+
+compare_df_cols(
+  Trips_Apr20,
+  Trips_May20,
+  Trips_Jun20,
+  Trips_Jul20,
+  Trips_Aug20,
+  Trips_Sep20,
+  Trips_Oct20,
+  Trips_Nov20,
+  Trips_Dec20,
+  Trips_Jan21,
+  Trips_Feb21,
+  Trips_Mar21,
+  Trips_Apr21,
+  return = "mismatch"
+)
+
+#Make datatype consistent
+
+Trips_Apr20 <- mutate(
+  Trips_Apr20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_May20 <- mutate(
+  Trips_May20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Jun20 <- mutate(
+  Trips_Jun20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Jul20 <- mutate(
+  Trips_Jul20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Aug20 <- mutate(
+  Trips_Aug20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Sep20 <- mutate(
+  Trips_Sep20,
+  end_station_id =
+    as.character(end_station_id),
+    as.character(start_station_id),
+  start_station_id =
+)
+
+Trips_Oct20 <- mutate(
+  Trips_Oct20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Nov20 <- mutate(
+  Trips_Nov20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Dec20 <- mutate(
+  Trips_Dec20,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Jan21 <- mutate(
+  Trips_Jan21,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+
+Trips_Feb21 <- mutate(
+  Trips_Feb21,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+Trips_Mar21 <- mutate(
+  Trips_Mar21,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+
+Trips_Apr21 <- mutate(
+  Trips_Apr21,
+  end_station_id =
+    as.character(end_station_id),
+  start_station_id =
+    as.character(start_station_id)
+)
+
+compare_df_cols(
+  Trips_Apr20,
+  Trips_May20,
+  Trips_Jun20,
+  Trips_Jul20,
+  Trips_Aug20,
+  Trips_Sep20,
+  Trips_Oct20,
+  Trips_Nov20,
+  Trips_Dec20,
+  Trips_Jan21,
+  Trips_Feb21,
+  Trips_Mar21,
+  Trips_Apr21,
+  return = "mismatch"
+)
+
+
+all_trips <-
+  bind_rows(
+    Trips_Apr20,
+    Trips_May20,
+    Trips_Jun20,
+    Trips_Jul20,
+    Trips_Aug20,
+    Trips_Sep20,
+    Trips_Oct20,
+    Trips_Nov20,
+    Trips_Dec20,
+    Trips_Jan21,
+    Trips_Feb21,
+    Trips_Mar21,
+    Trips_Apr21
+  )
+
+all_trips <- all_trips %>% rename(
+  trips_id = ride_id,
+  ride_type =
+    rideable_type
+  ,
+  start_time = started_at,
+  end_time = ended_at
+  ,
+  from_station_name = start_station_name
+  ,
+  from_station_id = start_station_id
+  ,
+  to_station_name = end_station_name
+  ,
+  to_station_id = end_station_id
+  ,
+  user_type = member_casual
+)
+
+
+
+colnames(all_trips)
+
+dim(all_trips)
+head(all_trips)
+
+str(all_trips)
+
+summary(all_trips)
+skim(all_trips)
+
+all_trips$date <- as.Date(all_trips$start_time)
+all_tripsmonth <- format(as.Date(all_trips$date), "%m")
+all_trips$day <- format(as.Date(all_trips$date), "%d")
+all_trips$year <- format(as.Date(all_trips$date), "%Y")
+all_trips$day_of_week <- format(as.Date(all_trips$date), "%A")
+
+all_trips$ride_length <-
+  difftime(all_trips$end_time, all_trips$start_time)
+
+is.factor(all_trips$ride_length)
+
+
+all_trips$ride_length <-
+  as.numeric(as.character(all_trips$ride_length))
+is.numeric
+
+skim(all_trips$ride_length)
+
+all_trips_v2 <- all_trips[!(all_trips$ride_length < 0),]
+skim(all_trips_v2)
+
+summary(all_trips_v2$ride_length)
+
+head(all_trips_v2)
+View(all_trips_v2)
+
+
+gc()
+
+write.csv(all_trips_v2, "data.csv")
